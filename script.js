960340dee0ee3ab70f3b42f0100529a50c43de4d
Lewis <lewisholliday@hotmail.co.uk>
2021-10-26 20:55:12 +0100

    brief session creating constructor for game input

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function Book() {
-    // the constructor...
+// the constructor...
+function gamePlayed(title, gametime, genre) {
+    this.title = title
+    this.gametime = gametime
+    this.genre = genre
+    this.gameInfo = function() {
+      /* need to implement space in string */
+      return ['You have played ' + genre + '' + title + ' for ' + gametime + ' hours.']
+    }
+    
   }
+
+  const game1 = new gamePlayed('cod', '200', 'shooter')
   
   function addBookToLibrary() {
     // do stuff here
