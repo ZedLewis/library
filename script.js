5d8be03578da83f80c35f8c1b10c04bafc8fbe5a
Lewis <lewisholliday@hotmail.co.uk>
2021-11-01 20:19:59 +0000

    Rearranged the styling slightly, moving focus onto the JS for the time being. Starting to build classes

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-const addBook = document.getElementById('addBook')
+const addGame = document.getElementById('addGame')
 
-
-// the constructor...
-function gamePlayed(title, gametime, genre) {
+// Book class
+class book {
+  constructor(title, gametime, genre) {
     this.title = title
     this.gametime = gametime
     this.genre = genre
     this.gameInfo = function() {
       return ['You have played ' + genre + ' game ' + title + ' for ' + gametime + ' hours.']
     }
-    
   }
+}
+
+// UI class
+class UI {
+  static displayBooks() {
+    const storedGames = [
+      {
+        title: 'Football Manager';
+        gametime: '200';
+        genre: 'Simulation'
+      }
+    ]
+    const games = storedGames
+
+    games.forEach((book) => UI.addBookToList(book))
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#gameList')
+  }
+}
+
+// Store class
+
+// Event: Display book
+
+// Event: Add book
+
+// Event: Remove book
+
 
   const game1 = new gamePlayed('cod', '200', 'shooter')
   
   function addBookToLibrary() {
     // do stuff here
   }
+
+  addGame.addEventListener('click', () => {
+      
+      console.log("clicked")
+    })
+  
